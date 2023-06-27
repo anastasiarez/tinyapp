b4a080ceb153cc7c26ebfa70f5b64571a1634d90
Mona Lisa <anastasiia.rn@gmail.com>
2023-06-27 16:04:05 -0400

    added html to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.get("/urls.json", (req, res) => { //adding routes
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => { //adding routes
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars); // urls_index is the name or path of the template file to be rendered. It could be a relative path to a specific template file, such as "views/urls_index.ejs", or simply the name of the template file
+});
+
 app.get("/hello", (req, res) => { //sending html
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
